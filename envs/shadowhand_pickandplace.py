629a80eec50064da80f7705bbf45bd539205e61a
= <martin.schuck@tum.de>
2022-03-15 10:56:24 +0100

    Add grasp start to SHPickAndPlace

--- envs/shadowhand_pickandplace.py
+++ envs/shadowhand_pickandplace.py
@@ -1,5 +1,6 @@
 """ShadowHandPickAndPlace class file."""
 from pathlib import Path
+import copy
 from typing import Dict
 from gym import utils
 import numpy as np
@@ -44,7 +45,7 @@ DEFAULT_INITIAL_QPOS = {
 class ShadowHandPickAndPlace(envs.robot_env.RobotEnv, utils.EzPickle):
     """Environment for pick and place with the ShadowHand."""
 
-    def __init__(self, reward_type: str = "sparse"):
+    def __init__(self, reward_type: str = "sparse", p_grasp_start: float = 0.):
         """Initialize the Mujoco sim."""
         self.c_low = (1.05, 0.4, 0.4)
         self.c_high = (1.55, 1.1, 0.4)
@@ -53,14 +54,15 @@ class ShadowHandPickAndPlace(envs.robot_env.RobotEnv, utils.EzPickle):
         self.target_in_the_air = True
         self.target_range = 0.15
         self.target_offset = 0.0
-        self.gripper_extra_height = 0.30
+        self.gripper_extra_height = 0.35
         self.reward_type = reward_type
         self.obj_range = 0.15
+        self.p_grasp_start = p_grasp_start
         super().__init__(model_path=MODEL_XML_PATH,
                          n_substeps=20,
                          n_actions=23,
                          initial_qpos=DEFAULT_INITIAL_QPOS)
-        utils.EzPickle.__init__(self, reward_type=reward_type)
+        utils.EzPickle.__init__(self, reward_type=reward_type, p_grasp_start=p_grasp_start)
 
     def _sample_goal(self) -> np.ndarray:
         goal = self.initial_gripper_xpos[:3] + self.np_random.uniform(
@@ -142,24 +144,72 @@ class ShadowHandPickAndPlace(envs.robot_env.RobotEnv, utils.EzPickle):
             return -d
 
     def _env_setup(self, initial_qpos: np.ndarray):
+        if self.p_grasp_start > 0:
+            self._env_setup_grasp(initial_qpos)
+            self.initial_state_grasp = copy.deepcopy(self.sim.get_state())
         for name, value in initial_qpos.items():
             self.sim.data.set_joint_qpos(name, value)
         envs.utils.reset_mocap_welds(self.sim)
         self.sim.forward()
 
         # Move end effector into position.
-        gripper_target = np.array([-0.52, 0.005, -0.431 + self.gripper_extra_height
-                                  ]) + self.sim.data.get_site_xpos("robot0:grip")  # noqa: E124
+        gripper_target = np.array([1.32, 0.75, 0.355 + self.gripper_extra_height])
         gripper_rotation = np.array([1.0, 0.0, 1.0, 0.0])
         self.sim.data.set_mocap_pos("robot0:mocap", gripper_target)
         self.sim.data.set_mocap_quat("robot0:mocap", gripper_rotation)
         for _ in range(10):
             self.sim.step()
-
         self.initial_gripper_xpos = self.sim.data.get_site_xpos("robot0:grip").copy()
         self.height_offset = self.sim.data.get_site_xpos("object0")[2]
 
+    def _env_setup_grasp(self, initial_qpos: np.ndarray):
+        for name, value in initial_qpos.items():
+            self.sim.data.set_joint_qpos(name, value)
+        envs.utils.reset_mocap_welds(self.sim)
+        self.sim.forward()
+        # Move end effector into position and configuration.
+        hand_ctrl = np.array(
+            [1, -1, 0, -1, -1, 0, -1, -1, 0, -1, -1, -1, 0, -1, -1, 0, 1, -1, -1, 0])
+        ctrl_range = self.sim.model.actuator_ctrlrange
+        act_range = (ctrl_range[:, 1] - ctrl_range[:, 0]) / 2.0
+        act_center = (ctrl_range[:, 1] + ctrl_range[:, 0]) / 2.0
+        self.sim.data.ctrl[:] = act_center + hand_ctrl * act_range
+        gripper_target = np.array([1.32, 0.75, 0.355 + self.gripper_extra_height])
+        gripper_rotation = np.array([1.0, 0.0, 1.0, 0.0])
+        self.sim.data.set_mocap_pos("robot0:mocap", gripper_target)
+        self.sim.data.set_mocap_quat("robot0:mocap", gripper_rotation)
+        for _ in range(10):
+            self.sim.step()
+        # Set object position below gripper
+        object_xpos = self.sim.data.get_site_xpos("robot0:grip")[:2] + np.array([0.052, 0.05])
+        object_qpos = self.sim.data.get_joint_qpos("object0:joint")
+        assert object_qpos.shape == (7,)
+        object_qpos[:2] = object_xpos
+        self.sim.data.set_joint_qpos("object0:joint", object_qpos)
+        self.sim.forward()
+        # Predefined grasp sequence
+        for i in range(30):
+            if i < 10:
+                action = np.array([0, 0, -0.3])
+                hand_ctrl = np.array(
+                    [1, -1, 0, -1, -1, 0, -1, -1, 0, -1, -1, -1, 0, -1, -1, 0, 1, -1, -1, 0])
+            elif i < 20:
+                action = np.array([0, 0, 0])
+                hand_ctrl = -np.array(
+                    [1, 1, 0, -.5, 0, 0, 0, 0, 0, -.5, -.5, -.5, 0, -.5, 1, 0, -1, 1, 1, 0])
+            else:
+                action = np.array([0, 0, 0.5])
+                hand_ctrl = -np.array(
+                    [1, 1, 0, -.5, 0, 0, 0, 0, 0, -.5, -.5, -.5, 0, -.5, 1, 0, -1, 1, 1, 0])
+            action = np.concatenate((action * 0.05, np.array([1., 0., 1., 0.])))
+            envs.utils.mocap_set_action(self.sim, action)
+            self.sim.data.ctrl[:] = act_center + hand_ctrl * act_range
+            self.sim.data.ctrl[:] = np.clip(self.sim.data.ctrl, ctrl_range[:, 0], ctrl_range[:, 1])
+            self.sim.step()
+
     def _reset_sim(self) -> bool:
+        if np.random.rand() < self.p_grasp_start:
+            return self._reset_sim_grasp()
         self.sim.set_state(self.initial_state)
         # Randomize start position of object.
         object_xpos = self.initial_gripper_xpos[:2]
@@ -170,6 +220,11 @@ class ShadowHandPickAndPlace(envs.robot_env.RobotEnv, utils.EzPickle):
         assert object_qpos.shape == (7,)
         object_qpos[:2] = object_xpos
         self.sim.data.set_joint_qpos("object0:joint", object_qpos)
+        self.sim.forward()
+        return True
+
+    def _reset_sim_grasp(self) -> bool:
+        self.sim.set_state(self.initial_state_grasp)
         self.sim.forward()
         return True
 
