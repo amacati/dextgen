9bdedebf8f208614ad52c31ff413745e4155cd18
= <martin.schuck@tum.de>
2022-08-08 15:23:28 +0200

    Fix SeaClear to use opposing objects and goals

--- envs/seaclear/seaclear.py
+++ envs/seaclear/seaclear.py
@@ -1,4 +1,5 @@
 """SeaClear submarine environment module."""
+import logging
 from typing import Dict
 from pathlib import Path
 
@@ -11,6 +12,8 @@ from envs.utils import goal_distance
 
 DEFAULT_MODEL_XML_PATH = str(Path("SeaClear", "seaclear.xml"))
 FANCY_MODEL_XML_PATH = str(Path("SeaClear", "seaclear_fancy.xml"))
+
+logger = logging.getLogger(__name__)
 
 
 class SeaClear(FlatBase):
@@ -26,8 +29,10 @@ class SeaClear(FlatBase):
             fancy_world: Loads a version with plants for visualization if True.
         """
         self.mocap_offset = np.array([0., 0., 0.])
-        self._obs_dist = 0.15
-        self.obs_threshold = 0.1
+        self._obs_threshold = 0.15
+        self._obs_range = 0.05  # Inner obstacle range for opposing goal and object positions
+        self._obs_violation = False
+        self._opposing_goal_object = False
         model_xml_path = FANCY_MODEL_XML_PATH if fancy_world else DEFAULT_MODEL_XML_PATH
         super().__init__(model_xml_path=model_xml_path,
                          gripper_extra_height=0.45,
@@ -39,6 +44,9 @@ class SeaClear(FlatBase):
         self.height_offset = 0.0
         self.target_range = 0.3
         self.gripper_init_range = 0.25
+        self.gripper_init_pos = np.array([0., 0., 0.45])
+        self.current_object_pose = None
+        # If true, use goal and object positions on opposite sides of the forbidden region
 
     def _set_action(self, action: np.ndarray):
         assert action.shape == (self.n_actions,)
@@ -76,7 +84,11 @@ class SeaClear(FlatBase):
         # gripper state
         grip_state = robot_qpos[-2:]
 
-        achieved_goal = object_pos
+        # If the object is inside the obstacle, sample a goal where no collision occurs
+        obs_pos = self.goal[3:6].copy()
+        while goal_distance(obs_pos, object_pos) < self._obs_threshold:
+            obs_pos[:2] = self.np_random.uniform(-self.target_range, self.target_range, size=2)
+        achieved_goal = np.concatenate((object_pos, obs_pos))
 
         obs = np.concatenate([
             grip_pos,
@@ -93,7 +105,7 @@ class SeaClear(FlatBase):
 
         return {
             "observation": obs.copy(),
-            "achieved_goal": np.concatenate((achieved_goal, self.goal[3:9])),
+            "achieved_goal": achieved_goal,
             "desired_goal": self.goal.copy(),
         }
 
@@ -105,11 +117,9 @@ class SeaClear(FlatBase):
             goal: Desired goal.
         """
         goal_d = goal_distance(achieved_goal[..., :3], goal[..., :3])
-        obstacle_d1 = goal_distance(achieved_goal[..., :3], goal[..., 3:6])
-        obstacle_d2 = goal_distance(achieved_goal[..., :3], goal[..., 6:9])
+        obstacle_d = goal_distance(achieved_goal[..., :3], goal[..., 3:6])
         goal_reward = -(goal_d > self.target_threshold).astype(np.float32)
-        obstacle_violation = (obstacle_d1 < self.obs_threshold) | (obstacle_d2 < self.obs_threshold)
-        obstacle_reward = -(obstacle_violation).astype(np.float32)
+        obstacle_reward = -(obstacle_d < self._obs_threshold).astype(np.float32)
         return goal_reward + obstacle_reward
 
     def _render_callback(self):
@@ -119,14 +129,25 @@ class SeaClear(FlatBase):
         # Visualize obstacles
         site_id = self.sim.model.site_name2id("obstacle0")
         self.sim.model.site_pos[site_id] = self.goal[3:6]
-        site_id = self.sim.model.site_name2id("obstacle1")
-        self.sim.model.site_pos[site_id] = self.goal[6:9]
         self.sim.forward()
+
+    def _step_callback(self):
+        object_pos = self.sim.data.get_site_xpos(self.object_name)
+        goal_d = goal_distance(object_pos[:3], self.goal[3:6])
+        self._obs_violation = goal_d > self._obs_threshold and self._obs_violation
 
     def _set_object_pose(self):
         object_pose = self.sim.data.get_joint_qpos(self.object_name + ":joint")
-        object_pose[:2] = self.np_random.uniform(-self.target_range, self.target_range, size=2)
-        object_pose[2] = 0.04
+        if self._opposing_goal_object:
+            # Calculate the object position in spherical coordinates around the env center
+            object_angle = 2 * (self.np_random.rand() - 0.5) * np.pi
+            object_range = self._obs_threshold + 0.05 + self.np_random.rand() * 0.05
+            object_pose[:2] = np.array([np.cos(object_angle), np.sin(object_angle)]) * object_range
+            object_pose[2] = 0.04
+        else:
+            object_pose[:2] = self.np_random.uniform(-self.target_range, self.target_range, size=2)
+            object_pose[2] = 0.04
+        self.current_object_pose = object_pose
         self.sim.data.set_joint_qpos(self.object_name + ":joint", object_pose)
 
     def _set_gripper_pose(self):
@@ -142,32 +163,46 @@ class SeaClear(FlatBase):
 
     def _reset_sim(self) -> bool:
         self.sim.set_state(self.initial_state)
+        self._opposing_goal_object = self.np_random.rand() > 0.5
         self._env_setup(self.initial_qpos)
+        self._obs_violation = False
         return True
 
     def _sample_goal(self) -> np.ndarray:
-        goal = np.zeros(9)
-        goal[:2] = self.np_random.uniform(-self.target_range, self.target_range, size=2)
-        goal[2] = self.np_random.uniform(0.2, 0.45)
-        for i in range(2):
-            obstacle = self.np_random.uniform(-self.target_range, self.target_range, size=3)
-            obstacle[2] = 0.05
-            object_pos = self.sim.data.get_site_xpos(self.object_name)
-            d_obj = goal_distance(obstacle[:2], object_pos[:2])
-            d_goal = goal_distance(obstacle[:2], goal[:2])
-            d_grip = goal_distance(obstacle[:2], self.gripper_start_pos[:2])
-            while any([d < self._obs_dist for d in (d_obj, d_goal, d_grip)]):
-                obstacle = self.np_random.uniform(-self.target_range, self.target_range, size=3)
-                obstacle[2] = 0.05
-                d_obj = goal_distance(obstacle[:2], object_pos[:2])
-                d_goal = goal_distance(obstacle[:2], goal[:2])
-                d_grip = goal_distance(obstacle[:2], self.gripper_start_pos[:2])
-            goal[(i + 1) * 3:(i + 2) * 3] = obstacle
+        object_pos = self.sim.data.get_site_xpos(self.object_name)
+        goal = np.zeros(6)
+        if self._opposing_goal_object:
+            goal[:3] = -object_pos.copy()
+            goal[2] = 0.05
+            goal[3:5] = self.np_random.uniform(-self._obs_range, self._obs_range, size=2)
+            goal[5] = 0.05
+            d_obj = goal_distance(goal[3:6], object_pos[:3])
+            d_goal = goal_distance(goal[3:6], goal[:3])
+            d_grip = goal_distance(goal[3:6], self.gripper_start_pos[:6])
+            while any([d < self._obs_threshold for d in (d_obj, d_goal, d_grip)]):
+                goal[3:5] = self.np_random.uniform(-self._obs_range, self._obs_range, size=2)
+                d_obj = goal_distance(goal[3:6], object_pos[:3])
+                d_goal = goal_distance(goal[3:6], goal[:3])
+                d_grip = goal_distance(goal[3:6], self.gripper_start_pos[:6])
+        else:
+            goal[:3] = self.np_random.uniform(-self.target_range, self.target_range, size=3)
+            goal[2] = self.np_random.uniform(0.2, 0.4)
+            goal[3:5] = self.np_random.uniform(-self.target_range, self.target_range, size=2)
+            goal[5] = 0.05
+            d_obj = goal_distance(goal[3:6], object_pos[:3])
+            d_goal = goal_distance(goal[3:6], goal[:3])
+            d_grip = goal_distance(goal[3:6], self.gripper_start_pos[:6])
+            while any([d < self._obs_threshold for d in (d_obj, d_goal, d_grip)]):
+                goal[3:5] = self.np_random.uniform(-self.target_range, self.target_range, size=2)
+                d_obj = goal_distance(goal[3:6], object_pos[:3])
+                d_goal = goal_distance(goal[3:6], goal[:3])
+                d_grip = goal_distance(goal[3:6], self.gripper_start_pos[:6])
+        self._goal = goal
         return goal
 
     def _is_success(self, achieved_goal: np.ndarray, desired_goal: np.ndarray) -> bool:
         d = envs.utils.goal_distance(achieved_goal[:3], desired_goal[:3])
-        return (d < self.target_threshold).astype(np.float32)
+        return (d < self.target_threshold and self._obs_violation).astype(np.float32)
 
     def _env_setup(self, initial_qpos: np.ndarray):
         self._modify_object_size()
@@ -180,9 +215,6 @@ class SeaClear(FlatBase):
             self.sim.data.set_joint_qpos(name, value)
         envs.utils.reset_mocap_welds(self.sim)
         self.sim.forward()
-        # Save start positions on first run
-        if self.gripper_init_pos is None:
-            self.gripper_init_pos = np.array([0., 0., 0.45])
         # Move end effector into position
         self._set_gripper_pose()
         # Change object pose
