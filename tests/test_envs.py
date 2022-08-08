cf1941ed79a20678c0ce32a64ec030900cbe91ae
= <martin.schuck@tum.de>
2022-08-08 10:40:46 +0200

    Fix bugs introduced in optim merge

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -5,11 +5,10 @@ import gym
 import envs  # noqa: F401
 
 
-@pytest.mark.parametrize("env", [e for e in envs.available_envs if "SH" not in e])
+@pytest.mark.parametrize("env", [e for e in envs.available_envs])
 def test_env(env):
     env = gym.make(env)
     env.reset()
-    env.use_contact_info()
     for _ in range(env._max_episode_steps):
         env.step(env.action_space.sample())
 
@@ -17,6 +16,14 @@ def test_env(env):
 @pytest.mark.parametrize("env", [e for e in envs.available_envs if "SH" in e])
 def test_shadowhand_eigen(env):
     env = gym.make(env, n_eigengrasps=random.randint(1, 20))
+    env.reset()
+    for _ in range(env._max_episode_steps):
+        env.step(env.action_space.sample())
+
+
+@pytest.mark.parametrize("env", [e for e in envs.available_envs if "Cube" in e])
+def test_contact_info(env):
+    env = gym.make(env)
     env.reset()
     env.use_contact_info()
     for _ in range(env._max_episode_steps):
